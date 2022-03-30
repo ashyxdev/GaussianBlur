4a1cbf8f4b171dff7eff5f702781a1e3158129df
ashyxdev <44738386+ashyxdev@users.noreply.github.com>
2022-03-29 18:38:23 -0700

    Create Blur.java

---
+++ me/ashyx/blur/util/Blur.java
@@ -0,0 +1,78 @@
+package me.ashyx.blur.util.blur;
+
+
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.shader.Framebuffer;
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_ZERO;
+import static org.lwjgl.opengl.GL20.glUniform1;
+
+public class GaussianBlur implements Utils {
+
+    public static ShaderUtil blurShader = new ShaderUtil("Blur/gaussian.frag");
+
+    public static Framebuffer framebuffer = new Framebuffer(1, 1, false);
+
+
+    public static void setupUniforms(float dir1, float dir2, float radius) {
+        blurShader.setUniformi("textureIn", 0);
+        blurShader.setUniformf("texelSize", 1.0F / (float) mc.displayWidth, 1.0F / (float) mc.displayHeight);
+        blurShader.setUniformf("direction", dir1, dir2);
+        blurShader.setUniformf("radius", radius);
+
+        final FloatBuffer weightBuffer = BufferUtils.createFloatBuffer(256);
+        for (int i = 0; i <= radius; i++) {
+            weightBuffer.put(calculateGaussianValue(i, radius / 2));
+        }  
+
+        weightBuffer.rewind();
+        glUniform1(blurShader.getUniform("weights"), weightBuffer);
+    }
+
+
+
+
+    public static void renderBlur(float radius) {
+        GlStateManager.enableBlend();
+        GlStateManager.color(1, 1, 1, 1);
+        OpenGlHelper.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO);
+
+
+        framebuffer = RenderUtil.createFrameBuffer(framebuffer);
+
+        framebuffer.framebufferClear();
+        framebuffer.bindFramebuffer(true);
+        blurShader.init();
+        setupUniforms(1, 0, radius);
+
+        RenderUtil.bindTexture(mc.getFramebuffer().framebufferTexture);
+
+        ShaderUtil.drawQuads();
+        framebuffer.unbindFramebuffer();
+        blurShader.unload();
+
+        mc.getFramebuffer().bindFramebuffer(true);
+        blurShader.init();
+        setupUniforms(0, 1, radius);
+
+        RenderUtil.bindTexture(framebuffer.framebufferTexture);
+        ShaderUtil.drawQuads();
+        blurShader.unload();
+
+        RenderUtil.resetColor();
+        GlStateManager.bindTexture(0);
+    }
+  
+  public static float calculateGaussianValue(float x, float sigma) {
+        double PI = 3.141592653;
+        double output = 1.0 / Math.sqrt(2.0 * PI * (sigma * sigma));
+        return (float) (output * Math.exp(-(x * x) / (2.0 * (sigma * sigma))));
+    }
+
+}
