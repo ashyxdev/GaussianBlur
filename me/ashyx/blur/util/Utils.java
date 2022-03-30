983658238e39597109b8e79c1909885ecd7bef13
ashyxdev <44738386+ashyxdev@users.noreply.github.com>
2022-03-29 18:35:46 -0700

    Create Utils.java

---
+++ me/ashyx/blur/util/Utils.java
@@ -0,0 +1,9 @@
+package me.ashyx.blur.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+
+public interface Utils {
+    Minecraft mc = Minecraft.getMinecraft();
+    FontRenderer fr = mc.fontRendererObj;
+}
