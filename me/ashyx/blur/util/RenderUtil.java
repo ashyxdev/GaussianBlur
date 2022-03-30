1f085732ea9839b9e2db63b1605bab1dba1875cb
ashyxdev <44738386+ashyxdev@users.noreply.github.com>
2022-03-29 18:37:13 -0700

    Create RenderUtil.java

---
+++ me/ashyx/blur/util/RenderUtil.java
@@ -0,0 +1,43 @@
+package dev.client.tenacity.utils.render;
+
+
+
+
+
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.shader.Framebuffer;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.glScissor;
+
+public class RenderUtil implements Utils {
+
+
+    public static Framebuffer createFrameBuffer(Framebuffer framebuffer) {
+        if (framebuffer == null || framebuffer.framebufferWidth != mc.displayWidth || framebuffer.framebufferHeight != mc.displayHeight) {
+            if (framebuffer != null) {
+                framebuffer.deleteFramebuffer();
+            }
+            return new Framebuffer(mc.displayWidth, mc.displayHeight, true);
+        }
+        return framebuffer;
+    }
+
+
+ 
+    /**
+     * Bind a texture using the specified integer refrence to the texture.
+     *
+     * @see org.lwjgl.opengl.GL13 for more information about texture bindings
+     */
+    public static void bindTexture(int texture) {
+        glBindTexture(GL_TEXTURE_2D, texture);
+    }
+
+    
+}
