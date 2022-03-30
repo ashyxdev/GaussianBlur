45663b8f43b2d1e45370adc30635215c6c1cb59c
ashyxdev <44738386+ashyxdev@users.noreply.github.com>
2022-03-29 18:40:23 -0700

    Create ShaderUtil.java

---
+++ me/ashyx/blur/util/ShaderUtil.java
@@ -0,0 +1,136 @@
+package me.ashyx.blur.util;
+
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.util.ResourceLocation;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL20.GL_LINK_STATUS;
+
+public class ShaderUtil implements Utils {
+    private final int programID;
+
+    public ShaderUtil(String fragmentShaderLoc, String vertexShaderLoc) {
+        int program = glCreateProgram();
+        try {
+            int fragmentShaderID;
+            
+            fragmentShaderID = createShader(mc.getResourceManager().getResource(new ResourceLocation(fragmentShaderLoc)).getInputStream(), GL_FRAGMENT_SHADER);
+                    
+            
+            glAttachShader(program, fragmentShaderID);
+
+            int vertexShaderID = createShader(mc.getResourceManager().getResource(new ResourceLocation(vertexShaderLoc)).getInputStream(), GL_VERTEX_SHADER);
+            glAttachShader(program, vertexShaderID);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        glLinkProgram(program);
+        int status = glGetProgrami(program, GL_LINK_STATUS);
+
+        if (status == 0) {
+            throw new IllegalStateException("Shader failed to link!");
+        }
+        this.programID = program;
+    }
+
+    public ShaderUtil(String fragmentShaderLoc) {
+        this(fragmentShaderLoc, "Blur/vertex.vsh");
+    }
+
+
+    public void init() {
+        glUseProgram(programID);
+    }
+
+    public void unload() {
+        glUseProgram(0);
+    }
+
+    public int getUniform(String name) {
+        return glGetUniformLocation(programID, name);
+    }
+
+
+    public void setUniformf(String name, float... args) {
+        int loc = glGetUniformLocation(programID, name);
+        switch (args.length) {
+            case 1:
+                glUniform1f(loc, args[0]);
+                break;
+            case 2:
+                glUniform2f(loc, args[0], args[1]);
+                break;
+            case 3:
+                glUniform3f(loc, args[0], args[1], args[2]);
+                break;
+            case 4:
+                glUniform4f(loc, args[0], args[1], args[2], args[3]);
+                break;
+        }
+    }
+
+    public void setUniformi(String name, int... args) {
+        int loc = glGetUniformLocation(programID, name);
+        if (args.length > 1) glUniform2i(loc, args[0], args[1]);
+        else glUniform1i(loc, args[0]);
+    }
+
+    public static void drawQuads(float x, float y, float width, float height) {
+        if (mc.gameSettings.ofFastRender) return;
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 0);
+        glVertex2f(x, y);
+        glTexCoord2f(0, 1);
+        glVertex2f(x, y + height);
+        glTexCoord2f(1, 1);
+        glVertex2f(x + width, y + height);
+        glTexCoord2f(1, 0);
+        glVertex2f(x + width, y);
+        glEnd();
+    }
+
+    public static void drawQuads() {
+        if (mc.gameSettings.ofFastRender) return;
+        ScaledResolution sr = new ScaledResolution(mc);
+        float width = (float) sr.getScaledWidth_double();
+        float height = (float) sr.getScaledHeight_double();
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 1);
+        glVertex2f(0, 0);
+        glTexCoord2f(0, 0);
+        glVertex2f(0, height);
+        glTexCoord2f(1, 0);
+        glVertex2f(width, height);
+        glTexCoord2f(1, 1);
+        glVertex2f(width, 0);
+        glEnd();
+    }
+
+    private int createShader(InputStream inputStream, int shaderType) {
+        int shader = glCreateShader(shaderType);
+        glShaderSource(shader, FileUtils.readInputStream(inputStream));
+        glCompileShader(shader);
+
+
+        if (glGetShaderi(shader, GL_COMPILE_STATUS) == 0) {
+            System.out.println(glGetShaderInfoLog(shader, 4096));
+            throw new IllegalStateException(String.format("Shader (%s) failed to compile!", shaderType));
+        }
+
+        return shader;
+    }
+
+
+   
+
+}
